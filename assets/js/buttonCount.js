6f66d7a7a8dc1d6b746d1198e75f3452d30dc2f1
Alexander Aranda <alexandermarkets1@gmail.com>
2023-01-08 14:57:47 -0500

    Ready for Public

--- assets/js/buttonCount.js
+++ assets/js/buttonCount.js
@@ -2,6 +2,11 @@ var counterContainer = document.querySelector(".website-counter");
 var resetButton = document.querySelector("#reset");
 var visitCount = localStorage.getItem("page_view");
 
+
+let lightTheme = "assets/css/style.css";
+let darkTheme = "assets/css/dark.css";
+
+
 // Check if page_view entry is present
 if (visitCount) {
   visitCount = Number(visitCount) + 1;
@@ -18,3 +23,18 @@ resetButton.addEventListener("click", () => {
   localStorage.setItem("page_view", 1);
   counterContainer.innerHTML = visitCount;
 });
+
+// Swaps the stylesheet in order to  achieve dark mode.
+function changeTheme() {
+  let darkMode = document.getElementById("dark-mode");
+  let theme = document.getElementById("theme");
+
+  if (theme.getAttribute("href") === darkTheme) {
+    theme.href = lightTheme;
+    darkMode.innerHTML = "Dark Mode 🌙";
+  } else {
+    theme.href = darkTheme;
+    darkMode.innerHTML = "Light Mode 🌞";
+  }
+
+}
